1964c96b3c74d903684627740643aef4a60b95cb
Julius Liu <juliusl@microsoft.com>
2022-04-05 19:46:54 -0700

    Feat. plumbing Doc. design doc for anatomy of definition file

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,50 @@
 use serde_yaml::Value;
 
 fn main() {
-    let object = yaml(r#"
+    let config = yaml(r#"
 tools:
 - az_cli:
   - az create vm
+  - az create network
 - cloud_init:
   - install-kind.yml:jinja2
 "#);
 
-    if let Some(tools) = object.get("tools").and_then(|f| f.as_sequence()) {
+    let installed_tools = vec![
+        AzCli{}.install(), 
+        CloudInit{}.install()
+    ];
+
+    let referenced_tools = parse_tools(config, installed_tools);
+
+    // prints [Tool { name: "az_cli", data: ["az create vm", "az create network"] }, Tool { name: "cloud_init", data: ["install-kind.yml:jinja2"] }]
+    println!("{:?}", referenced_tools)
+}
+
+
+/// Parse a YAML definition blob and extract tool settings
+pub fn parse_tools(object: serde_yaml::Value, installed: Vec<&str>) -> Vec<Tool> {
+    let mut referenced_tools: Vec<Tool> = vec![];
+    
+    if let Some(tools) = object.get("tools").and_then(Value::as_sequence) {
         for t in tools {
-            if let Some(tool) = t.get("az_cli") {
-                if let Some(cmds) = tool.as_sequence() {
-                    println!("az-cli: {:?}", cmds);
-                }
-            } else if let Some(tool) = t.get("cloud_init") {
-                if let Some(cmds) = tool.as_sequence() {
-                    println!("cloud-init: {:?}", cmds);
+            if let Some(tool) = installed.iter().find(|v| t.get(**v).is_some()) {
+                if let Some(settings) = t.get(*tool).and_then(Value::as_sequence) {
+                  let settings: Vec<String> = settings.iter()
+                    .filter_map(Value::as_str)
+                    .map(str::to_string)
+                    .collect();
+
+                    referenced_tools.push(Tool {
+                        name: tool.to_string(),
+                        data: settings,
+                    });
                 }
             }
         }
     }
+
+    referenced_tools
 }
 
 fn yaml(i: &str) -> serde_yaml::Value { serde_yaml::from_str(i).unwrap() }
@@ -35,7 +58,8 @@ struct Component {
     requires: Vec<SettingData>,
 }
 
-struct Tool {
+#[derive(Debug)]
+pub struct Tool {
     name: String,
     data: Vec<String>,
 }
@@ -45,8 +69,37 @@ struct SettingData {
     value: Option<String>
 }
 
-trait Tooling {
-    fn init(tool: Tool) -> Self;
 
-    fn output(&self) -> Self;
+/// Built in az cli tool 
+struct AzCli;
+
+impl Tooling for AzCli {
+    fn install(&self) -> &str {
+        "az_cli"
+    }
+
+    fn init(&self, tool: Tool) -> Self {
+        todo!()
+    }
+}
+
+/// Built in cloud init tool
+struct CloudInit;
+
+impl Tooling for CloudInit {
+    fn install(&self) -> &str {
+        "cloud_init"
+    }
+
+    fn init(&self, tool: Tool) -> Self {
+        todo!()
+    }
+}
+
+trait Tooling {
+    /// install returns the setting key for this tool
+    fn install(&self) -> &str;
+
+    /// initialize tooling from the tool settings
+    fn init(&self, tool: Tool) -> Self;
 }
