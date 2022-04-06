547e025327cc7b873a22b4f1d82a75f9d647de92
Julius Liu <juliusl@microsoft.com>
2022-04-05 19:00:42 -0700

    Experiment with some yamlage

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,52 @@
+use serde_yaml::Value;
+
 fn main() {
-    dependencies();
+    let object = yaml(r#"
+tools:
+- az_cli:
+  - az create vm
+- cloud_init:
+  - install-kind.yml:jinja2
+"#);
+
+    if let Some(tools) = object.get("tools").and_then(|f| f.as_sequence()) {
+        for t in tools {
+            if let Some(tool) = t.get("az_cli") {
+                if let Some(cmds) = tool.as_sequence() {
+                    println!("az-cli: {:?}", cmds);
+                }
+            } else if let Some(tool) = t.get("cloud_init") {
+                if let Some(cmds) = tool.as_sequence() {
+                    println!("cloud-init: {:?}", cmds);
+                }
+            }
+        }
+    }
 }
 
-/// Check that required dependencies are installed
-fn dependencies() {
-    std::process::Command::new("ssh").output().expect("ssh is not installed");
-    std::process::Command::new("az").output().expect("az cli is not installed");
-    std::process::Command::new("cloud-init").output().expect("cloud-init is not installed");
+fn yaml(i: &str) -> serde_yaml::Value { serde_yaml::from_str(i).unwrap() }
+
+struct Component {
+    id: String,
+    display_name: Option<String>,
+    description: Option<String>,
+    depends_on: Vec<String>,
+    tools: Vec<Tool>,
+    requires: Vec<SettingData>,
+}
+
+struct Tool {
+    name: String,
+    data: Vec<String>,
+}
+
+struct SettingData {
+    name: String,
+    value: Option<String>
+}
+
+trait Tooling {
+    fn init(tool: Tool) -> Self;
+
+    fn output(&self) -> Self;
 }
