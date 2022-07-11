86dfa2cc98f2501316239bf8080549f10404ce4f
Julius Liu <juliusl@microsoft.com>
2022-07-10 18:06:18 -0700

    nit formatting

--- lib/elm/portal/src/Editor.elm
+++ lib/elm/portal/src/Editor.elm
@@ -1,15 +1,17 @@
 module Editor exposing (viewCodeEditor)
 
 import Element exposing (..)
-import Element.Input
 import Element.Font as Font
+import Element.Input
 import Html exposing (node)
 import Html.Attributes exposing (attribute)
+
 
 type alias Model =
     { text : String, language : String }
 
-viewCodeEditor : { onDispatchSave: msg, onSave: (String -> msg) } -> { enableMonaco : Bool, visible: Bool } -> { language : String, text : String } -> Element msg
+
+viewCodeEditor : { onDispatchSave : msg, onSave : String -> msg } -> { enableMonaco : Bool, visible : Bool } -> { language : String, text : String } -> Element msg
 viewCodeEditor msgs settings editor =
     let
         visible =
@@ -17,28 +19,36 @@ viewCodeEditor msgs settings editor =
 
         enableMonaco =
             settings.enableMonaco
+
         onDispatch =
             msgs.onDispatchSave
+
         onSave =
             msgs.onSave
     in
-    if visible then 
-            if enableMonaco then
-                Element.column [ 
-                    width (fill 
+    if visible then
+        if enableMonaco then
+            Element.column
+                [ width
+                    (fill
                         |> minimum 800
                         |> maximum 1080
-                    ), height fill ]
-                [   Element.Input.button []
+                    )
+                , height fill
+                ]
+                [ Element.Input.button []
                     { onPress = Just onDispatch
                     , label = Element.text "Render"
                     }
-                ,viewMonacoEditor editor
+                , viewMonacoEditor editor
                 ]
+
         else
-        viewMultilineEditor onSave editor
+            viewMultilineEditor onSave editor
+
     else
         Element.text ""
+
 
 viewMonacoEditor : Model -> Element msg
 viewMonacoEditor model =
@@ -46,14 +56,17 @@ viewMonacoEditor model =
         (node "code-editor"
             [ attribute "value" model.text
             , attribute "language" model.language
-            ] [ ]
+            ]
+            []
         )
+
 
 viewMultilineEditor : (String -> msg) -> Model -> Element msg
 viewMultilineEditor onChange model =
-    Element.Input.multiline 
+    Element.Input.multiline
         [ Font.size 14
-        , Font.family [ Font.monospace] ]
+        , Font.family [ Font.monospace ]
+        ]
         { onChange = onChange
         , text = model.text
         , placeholder = Nothing
