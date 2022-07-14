7b8e1ad3476937daa42d9cd7a8b8eb298217d4b2
Julius Liu <juliusl@microsoft.com>
2022-07-14 14:11:35 -0700

    fix make-vm script

--- lib/sh/make-vm.sh
+++ lib/sh/make-vm.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+echo "Creating VM with:"
 echo "resource_group: $RESOURCE_GROUP"
 echo "vm_name:        $VM_NAME"
 echo "image:          $IMAGE"
 echo "location:       $LOCATION"
 echo "admin_user:     $ADMIN_USERNAME"
-echo "dns:            $VM_NAME-$DEV_ID"
+echo "dns:            $VM_NAME-$DEV_ID.$LOCATION.cloudapp.azure.com"
 
 # Deploy VM
 IP_ADDRESS=$(az vm create \
@@ -19,17 +20,14 @@ IP_ADDRESS=$(az vm create \
   --generate-ssh-keys \
   --nic-delete-option delete \
   --os-disk-delete-option delete \
-  --custom-data user-data \
+  --custom-data user_data \
   --tags dev_id=$DEV_ID | jq -r .publicIpAddress)
 
 # Wait for the machine to be ready
 # Note: Things are happening in parallel, so wait a second before trying to connect
-sleep 5s
-echo "Waiting for the VM to finish Setup ($IP_ADDRESS)"
-AWK_TAIL_LOG='/block\t/ {print};/Fetched.+MB/{print "perf \t"$0};/MB.s.+saved/ {print "perf \t"$0};/Cloud-init.+finished.+Up/ {print;exit}'
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $IP_ADDRESS "tail -f /var/log/cloud-init-output.log | awk '$AWK_TAIL_LOG'"
+# sleep 5s
+# echo "Waiting for the VM to finish Setup ($IP_ADDRESS)"
+# AWK_TAIL_LOG='/block\t/ {print};/Fetched.+MB/{print "perf \t"$0};/MB.s.+saved/ {print "perf \t"$0};/Cloud-init.+finished.+Up/ {print;exit}'
+# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $IP_ADDRESS "tail -f /var/log/cloud-init-output.log | awk '$AWK_TAIL_LOG'"
 
-# The machine is ready to connect to
-# Note: closing an ssh session does weird things with the buffer, give it a second to close
-sleep 5s
-ssh $IP_ADDRESS
+echo "VM is ready at $IP_ADDRESS"
